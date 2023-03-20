82dccd090dfd50c455bd6fcccbdb2af6b4b1757c
harkanni <harkanzyempire001@gmail.com>
2023-03-20 23:46:01 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Javascript loaded")
+// SIDEBAR TOGGLE
+var PageObj = {
+	sidebarIsViscible: false,
+	SIDEBAR: document.querySelector(".sidebar"),
+	menuBar: document.querySelector(".hambugger-menu"),
+	togleBtn: document.querySelector(".togleBtn"),
+	Toggle_Sidebar: function (){
+		// console.log(this)
+		if(this.sidebarIsViscible == false){
+			this.SIDEBAR.classList.add("viscible")
+			this.SIDEBAR.setAttribute("data-menu", "on")
+			this.sidebarIsViscible = true
+		}
+		else {
+			this.SIDEBAR.classList.remove("viscible")	
+			this.sidebarIsViscible = false
+			this.SIDEBAR.setAttribute("data-menu", "on")
+		}
+		
+	},
+}
+
+// USED THE CALL METHOD TO SET THE "this" keyword to the PageObj
+PageObj.menuBar.addEventListener("click", () => {
+	PageObj.Toggle_Sidebar.call(PageObj)
+})
+PageObj.togleBtn.addEventListener("click", () => {
+	PageObj.Toggle_Sidebar.call(PageObj)
+})
