da9f80d2938f6af9da545fbda01550e015b0b83e
harkanni <harkanzyempire001@gmail.com>
2023-03-22 19:28:00 +0100

    backend architecture

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json())
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*")
+	next()
+})
+
+// app.use("/", (express.static("Public")))
+
+app.get("/bitly", (req, res) => {
+	console.log("Request received")
+	res.send("<h1>Hello WORLD</h1>")
+})
+
+
+app.listen(PORT, () => {
+	console.log(`App Listening on port ${PORT}`)
+})
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log("Javascript")
 var PageObj = {
+	getUrlBtn: document.querySelector("#submit"),
 	sidebarIsViscible: false,
+	URL: "http://localhost:8080/bitly",
 	SIDEBAR: document.querySelector(".sidebar"),
 	menuBar: document.querySelector(".hambugger-menu"),
 	togleBtn: document.querySelector(".togleBtn"),
@@ -12,17 +14,32 @@ var PageObj = {
 		}
 		else {
 			this.SIDEBAR.classList.remove("viscible")			
-			this.SIDEBAR.setAttribute("data-menu", "on")
+			this.SIDEBAR.setAttribute("data-menu", "off")
 			this.sidebarIsViscible = false;
 		}
 		
 	},
+	ShortenLink: async function(link){
+		await fetch(this.URL)
+		.then((res) => {
+			let data = res.json()
+			console.log(data)
+		})
+	}
 }
 
-// USED THE CALL METHOD TO SET THE "this" keyword to the PageObj
+// USED THE CALL METHOD TO SET THE "this" keyword to the PageObj Object;
 PageObj.menuBar.addEventListener("click", () => {
 	PageObj.Toggle_Sidebar.call(PageObj)
 })
 PageObj.togleBtn.addEventListener("click", () => {
 	PageObj.Toggle_Sidebar.call(PageObj)
 })
+PageObj.getUrlBtn.addEventListener("click", (e) => {
+	e.preventDefault()
+	console.log("click")
+	PageObj.ShortenLink.call(PageObj)
+})
+
+
+
